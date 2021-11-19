76c465d310a91a644b5fc74a24548ef1cc34406d
Birgit Schreiber <bixilein.fixilein@gmail.com>
2021-11-19 17:09:14 +0100

    Update visualization function for MobileNetV3

--- visualize_subnet.py
+++ visualize_subnet.py
@@ -1,86 +1,88 @@
 import matplotlib.pyplot as plt
-import numpy as np
-import utils
 
 
 class Visualisation:
     def __init__(self):
-        self.mbv3_base_stage_width = [16, 24, 24, 24, 24, 40, 40, 40, 40, 80, 80, 80, 80, 112, 112, 112, 112, 160, 160,
-                                      160, 160]
+        # variable layers
+        self.mbv3_large_block_widths = [16, 24, 24, 24, 24, 40, 40, 40, 40, 80, 80, 80, 80, 112, 112, 112, 112, 160,
+                                        160, 160]
+        # can only me used for ANNETTE latency estimation
+        self.mbv3_small_block_widths = [16, 24, 24, 24, 0, 40, 40, 40, 0, 48, 48, 48, 0, 96, 96, 96, 0, 0, 0, 0]
 
-    def mbv3_barchart_relative(self, subnet_config, save_path=None, title=None, show=False):
+    def mbv3_barchart(self, subnet_config, save_path=None, title=None, show=False, show_fixed=True, relative=True):
         layers = []
-        for i in range(1, 21):
+        bar_idx = []
+        bar_length = []
+        bar_height = []
+        bar_colour = []
 
+        if show_fixed:
+            bar_idx.append(1)
+            bar_height.append(3 / 8)
+            bar_colour.append('black')
+            if relative:
+                bar_length.append(100)
+            else:
+                bar_length.append(16)
+
+            bar_idx.append(2)
+            bar_height.append(3 / 8)
+            bar_colour.append('black')
+            if relative:
+                bar_length.append(100)
+            else:
+                bar_length.append(16)
+
+            idx = 2
+        else:
+            idx = 0
+
+        for i in range(1, 21):
             if i <= subnet_config['d'][int((i - 1) / 4)] + int((i - 1) / 4) * 4:
                 active = True
             else:
                 active = False
 
-            layers.append({'idx': i,
+            layers.append({'idx': idx + i,
                            'active': active,
-                           'e': subnet_config['e'][i - 1] * self.mbv3_base_stage_width[i - 1],
+                           'e': subnet_config['e'][i - 1] * self.mbv3_large_block_widths[i - 1],
                            'ks': subnet_config['ks'][i - 1]})
 
-        bar_idx = [layer['idx'] for layer in layers]
-        bar_length = []
-        bar_height = []
-        for idx in range(0, 20):  # TODO why tf is this list only 20 long?
+        bar_idx = bar_idx + [layer['idx'] for layer in layers]
+        for idx in range(0, 20):  # 20 layers are dynamic
             if layers[idx]['active']:
-                bar_length.append(layers[idx]['e'] / (self.mbv3_base_stage_width[idx] * 6 ) * 100)
+                if relative:
+                    bar_length.append(layers[idx]['e'] / (self.mbv3_large_block_widths[idx] * 6) * 100)
+                else:
+                    bar_length.append(layers[idx]['e'])
                 bar_height.append(layers[idx]['ks'] / 8)
+                bar_colour.append('blue')
+
             else:
                 bar_length.append(0)
                 bar_height.append(0)
+                bar_colour.append('blue')
 
-        fig, ax = plt.subplots()
-        plt.rcdefaults()
-
-        chart = plt.barh(bar_idx, bar_length, align='edge', alpha=0.5, color='blue')
-
-        for i, obj in enumerate(chart):
-            obj.set_height(bar_height[i])
-
-        ax.invert_yaxis()
-        ax.set_yticks(bar_idx)
-        ax.set_yticklabels(bar_idx)
-
-        if title is not None:
-            ax.set_title(title)
-        ax.set_ylabel('layer index')
-        ax.set_xlabel('number of channels')
-        ax.spines['top'].set_visible(False)
-        ax.spines['right'].set_visible(False)
-
-        if save_path is not None:
-            plt.savefig(save_path)
-        if show:
-            plt.show()
-
-        return fig
-
-    def mbv3_barchart(self, subnet_config, save_path=None, title=None, show=False):
-        layers = []
-        for i in range(1, 21):
-
-            if i <= subnet_config['d'][int((i - 1) / 4)] + int((i - 1) / 4) * 4:
-                active = True
+        if show_fixed:
+            bar_idx.append(23)
+            bar_height.append(1 / 8)
+            bar_colour.append('black')
+            if relative:
+                bar_length.append(100)
             else:
-                active = False
-
-            layers.append({'idx': i,
-                           'active': active,
-                           'e': subnet_config['e'][i - 1] * self.mbv3_base_stage_width[i - 1],
-                           'ks': subnet_config['ks'][i - 1]})
-
-        bar_idx = [layer['idx'] for layer in layers]
-        bar_length = [0 if not layer['active'] else layer['e'] for layer in layers]
-        bar_height = [0 if not layer['active'] else layer['ks'] / 8 for layer in layers]
+                bar_length.append(960)
+            bar_idx.append(24)
+            bar_height.append(1 / 8)
+            bar_colour.append('black')
+            if relative:
+                bar_length.append(100)
+            else:
+                bar_length.append(1280)
 
         fig, ax = plt.subplots()
         plt.rcdefaults()
 
-        chart = plt.barh(bar_idx, bar_length, align='edge', alpha=0.5, color='blue')
+        chart = plt.barh(bar_idx, bar_length, align='edge', alpha=0.5, color=bar_colour)
 
         for i, obj in enumerate(chart):
             obj.set_height(bar_height[i])
@@ -91,8 +93,11 @@ class Visualisation:
 
         if title is not None:
             ax.set_title(title)
-        ax.set_ylabel('layer index')
-        ax.set_xlabel('number of channels')
+        ax.set_ylabel('Layer index')
+        if relative:
+            ax.set_xlabel('Number of channels (relative to max number channels) [%]')
+        else:
+            ax.set_xlabel('Number of channels')
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
 
@@ -103,32 +108,35 @@ class Visualisation:
 
         return fig
 
+
+reference_networks = [
+    {
+        # 0 means that this block will be removed
+        'title': 'Default MobileNetV3 Large Architecture',
+        'config': {'ks': [3, 3, 3, 0, 0, 5, 5, 5, 0, 3, 3, 3, 3, 3, 3, 0, 0, 5, 5, 5],
+                   'e':  [1, 2.67, 3, 0, 0, 1.8, 3, 3, 0, 3, 2.5, 2.3, 2.3, 4.29, 6, 0, 0, 4.2, 6, 6],
+                   'd':  [2, 3, 4, 2, 3], 'r': 224},
+        'save_path': 'figures/ofa_mbv3_default.png',
+    },
+    {
+        'title': 'OFA Largest Network for MobileNetV3 Architecture',
+        'config': {'ks': [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+                   'e':  [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
+                   'd':  [4, 4, 4, 4, 4], 'r': 224},
+        'save_path': 'figures/ofa_mbv3_max.png',
+    },
+    {
+        # TODO use different base width for this one
+        'title': 'Default MobileNetV3 Small Architecture',
+        'config': {'ks': [3, 3, 3, 0, 0, 5, 5, 5, 0, 5, 5, 0, 0, 5, 5, 5, 0, 0, 0, 0],
+                   'e':  [1, 3, 3.67, 0, 0, 2.4, 6, 6, 0, 2.5, 3, 0, 0, 3, 6, 6, 0, 0, 0, 0],
+                   'd':  [2, 3, 2, 3, 0], 'r': 224},
+        'save_path': 'figures/ofa_mbv3_default.png',
+    },
+]
 
 if __name__ == '__main__':
-    test_graphics = [
-        {
-            'title': 'Default MobileNetV3 Large Architecture',
-            'config': {'ks': [3, 3, 3, 3, 5, 5, 5, 3, 3, 3, 3, 3, 3, 5, 5, 5, 1, 7, 1, 1],
-                       # TODO get right default config
-                       'e': [3, 4, 6, 4, 6, 6, 4, 4, 4, 3, 3, 4, 6, 4, 6, 6, 4, 4, 4, 6],
-                       'd': [2, 3, 4, 2, 3], 'r': 224},
-            'save_path': 'figures/ofa_mbv3_default.png',
-        },
-        {
-            'title': 'OFA Largest Network for MobileNetV3 Architecture',
-            'config': {'ks': [7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
-                       'e': [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6],
-                       'd': [4, 4, 4, 4, 4], 'r': 224},
-            'save_path': 'figures/ofa_mbv3_max.png',
-        },
-        {
-            'title': 'OFA Subnet MobileNetV3 Architecture',
-            'config': {'ks': [3, 5, 7, 3, 3, 3, 3, 3, 5, 5, 5, 5, 3, 3, 5, 3, 7, 5, 7, 5],
-                       'e': [3, 4, 6, 4, 6, 6, 4, 4, 4, 3, 3, 4, 6, 4, 6, 6, 4, 4, 4, 6],
-                       'd': [4, 2, 2, 3, 4], 'r': 192},
-            'save_path': 'figures/ofa_mbv3_subnet.png',
-        },
-    ]
 
     drawing = Visualisation()
-    drawing.mbv3_barchart(test_graphics[0]['config'], test_graphics[0]['save_path'], test_graphics[0]['title'])
+    drawing.mbv3_barchart(reference_networks[1]['config'], 'max_net.png', 'Largest OFA-MobileNetV3 Architecture', show=True,
+                          show_fixed=True, relative=True)
