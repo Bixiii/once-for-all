2c15dfcb3b26ced76b2b7704459cc87732c784ef
Birgit Schreiber <bixilein.fixilein@gmail.com>
2022-05-29 23:03:36 +0200

    WIP: Change AlexNet architecture to planned OFA-basenet architecture

--- ofa/imagenet_classification/networks/alexnet.py
+++ ofa/imagenet_classification/networks/alexnet.py
@@ -20,18 +20,34 @@ class AlexNet(MyNetwork):
         super(AlexNet, self).__init__()
 
         self.features = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=1),
             nn.ReLU(inplace=True),
+
+            nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(64, 192, kernel_size=3, padding=1),
+
+            nn.Conv2d(64, 192, kernel_size=5, padding=1),
             nn.ReLU(inplace=True),
+
+            nn.Conv2d(192, 192, kernel_size=5, padding=1),
+            nn.ReLU(inplace=True),
+
             nn.MaxPool2d(kernel_size=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+
+            nn.Conv2d(192, 384, kernel_size=5, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+
+            nn.Conv2d(384, 384, kernel_size=5, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+
+            nn.Conv2d(384, 256, kernel_size=5, padding=1),
             nn.ReLU(inplace=True),
+
+            nn.Conv2d(256, 256, kernel_size=5, padding=1),
+            nn.ReLU(inplace=True),
+
             nn.MaxPool2d(kernel_size=2),
         )
         self.classifier = nn.Sequential(
