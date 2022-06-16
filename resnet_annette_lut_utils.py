09fa5d81639d370e951c470a431dce2d19ae5d56
Birgit Schreiber <bixilein.fixilein@gmail.com>
2022-06-16 19:41:35 +0200

    Clean up script for creating ANNETTE look up table

--- resnet_annette_lut_utils.py
+++ resnet_annette_lut_utils.py
@@ -1,23 +1,23 @@
 from pathlib import Path
+import pickle
+import csv
+import time
+import pickle
+import random
+
+import onnx
+from onnxsim import simplify
 
 from annette import get_database
 from annette.estimation.layer_model import Layer_model
 from annette.estimation.mapping_model import Mapping_model
 from annette.graph import ONNXGraph, AnnetteGraph
-from onnxsim import simplify
-import pickle
-import csv
-import time
 
 from ofa.imagenet_classification.elastic_nn.networks import OFAResNets, OFAMobileNetV3
-import onnx
-
 from ofa.nas.efficiency_predictor import AnnetteLatencyLayerPrediction, AnnetteLatencyModelResNet50
-from utils import timestamp_string_ms, export_as_onnx, architecture_config_2_str, export_layer_as_onnx, export_as_simplified_onnx
+
+from utils import *
 from result_net_configs import *
-import pickle
-import random
-from result_net_configs import resnet50_flop_constrained
 
 test_config = {'d': [2, 2, 2, 2, 2],
                'e': [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25,
@@ -30,8 +30,10 @@ expand_list = [0.2, 0.25, 0.35]
 width_mult_list = [0.65, 0.8, 1.0]
 img_size_list = []
 small_input_stem = False
-target_hardware = 'dnndk-mixed'
-lut_file_path = r'C:\Users\bixi\PycharmProjects\OnceForAllFork\LUT_ofa_resnet_ncs2-mixed.pkl'
+
+target_hardware = 'dnndk-mixed'  # or ncs2-mixed
+lut_file_path = r'C:\Users\bixi\PycharmProjects\OnceForAllFork\LUT_ofa_resnet_dnndk-mixed.pkl'
+# lut_file_path = r'C:\Users\bixi\PycharmProjects\OnceForAllFork\LUT_ofa_resnet_ncs2-mixed.pkl'
 
 # crate OFA-network ResNet50
 ofa_network = OFAResNets(
@@ -44,42 +46,42 @@ ofa_network = OFAResNets(
     dataset='imagenet'
 )
 
+
+# build full ANNETTE LUT
+def build_annette_lut(annette_model, image_sizes=None, file_path=''):
+    if not image_sizes:
+        image_sizes = [128, 144, 160, 176, 192, 224, 240, 256]
+    latency_lut = ofa_network.build_annette_lut(image_sizes, annette_model, file_path)
+    return latency_lut
+
+
+# load ANNETTE LUT
+def load_annette_lut(file_path):
+    annette_latency_lut_file = open(file_path, 'rb')
+    annette_latency_lut = pickle.load(annette_latency_lut_file)
+    annette_latency_lut_file.close()
+    return annette_latency_lut
+
+
+# make single prediction with ANNETTE LUT
+def annette_latency_lut_prediction(subnet_config, annette_latency_lut):
+    start = time.time()
+    latency, layer_files = ofa_network.predict_with_annette_lut(annette_latency_lut, subnet_config)
+    end = time.time()
+    print("Latency is " + str(latency) + ", estimation time was " + str(int((end - start) * 1000)) + "ms")
+    return latency
+
+
 # # make prediction form ONNX with ANNETTE for a subnet
-# image_sizes = [128, 144, 160, 176, 192, 224, 240, 256]
-# image_size = 256
-# ofa_network.set_active_subnet(d=test_config['d'], e=test_config['e'], w=test_config['w'])
-# subnet = ofa_network.get_active_subnet()
-# annette_latency_predictor = AnnetteLatencyLayerPrediction()
-# latency_from_onnx = annette_latency_predictor.predict_efficiency(subnet, (1, 3, image_size, image_size))
-
-# # build full ANNETTE LUT
-# annette_model = 'ncs2-mixed'
-# image_sizes = [128, 144, 160, 176, 192, 224, 240, 256]
-# latency_lut = ofa_network.build_annette_lut(image_sizes, annette_model, '')
-
-# # load ANNETTE LUT
-annette_latency_lut_file = open(lut_file_path, 'rb')
-annette_latency_lut = pickle.load(annette_latency_lut_file)
-annette_latency_lut_file.close()
-
-# # make single prediction with ANNETTE LUT
-start = time.time()
-subnet_config = ofa_network.sample_active_subnet()
-image_size = random.choice([128])
-subnet_config['image_size'] = image_size
-latency, layer_files = ofa_network.predict_with_annette_lut(annette_latency_lut, subnet_config)
-end = time.time()
-print("Latency is " + str(latency) + ", estimation time was " + str(int((end-start)*1000)) + " ms")
-
-# # build single resolution ANNETTE LUT
-# image_size = 256
-# latency_lut = ofa_network.build_annette_lut(image_sizes=[image_size])
-
-# # add single resolution LUT to loaded lut
-# new_annette_latency_lut_file = open('new_full_restnet_lut.pkl', 'wb')
-# annette_latency_lut[image_size] = latency_lut[image_size]
-# pickle.dump(annette_latency_lut, new_annette_latency_lut_file)
-# new_annette_latency_lut_file.close()
+def annette_latency_onnx_prediction(subnet_config):
+    start = time.time()
+    ofa_network.set_active_subnet(d=subnet_config['d'], e=subnet_config['e'], w=subnet_config['w'])
+    subnet = ofa_network.get_active_subnet()
+    annette_latency_predictor = AnnetteLatencyLayerPrediction()
+    latency = annette_latency_predictor.predict_efficiency(subnet, (1, 3, subnet_config['image_size'], subnet_config['image_size']))
+    end = time.time()
+    print("Latency is " + str(latency) + ", estimation time was " + str(int((end - start) * 1000)) + "ms")
+    return latency
 
 
 # # evaluate ANNETTE LUT
@@ -108,4 +110,17 @@ print("Latency is " + str(latency) + ", estimation time was " + str(int((end-sta
 # results_csv_file.close()
 
 
+# file_name = 'LUT_ofa_resnet_' + target_hardware + '.pkl'
+# annette_latency_lut = load_annette_lut(file_name)
+#
+# subnet_config = ofa_network.sample_active_subnet()
+# image_sizes = [128, 144, 160, 176, 192, 224, 240, 256]
+# subnet_config['image_size'] = random.choice(image_sizes)
+#
+# latency, layer_files = ofa_network.predict_with_annette_lut(annette_latency_lut, subnet_config)
+# print(str(layer_files))
+#
+# subnet = ofa_network.get_active_subnet()
+# export_as_simplified_onnx(subnet, 'ofa-resnet-test-net.onnx', subnet_config['image_size'])
 
+build_annette_lut('dnndk-mixed')
