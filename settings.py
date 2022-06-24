6f57f95685063eb77cf51b99294be1cf25fdffb9
Birgit Schreiber <bixilein.fixilein@gmail.com>
2022-06-24 10:40:03 +0200

    Add option to disable ANNETTE-estimators

--- settings.py
+++ settings.py
@@ -1,2 +1,3 @@
-use_hvd = True  # deactivate usage of horovod
+use_hvd = False  # deactivate usage of horovod
 tiny_gpu = False  # adapt hyper-parameter, that I can run it on my local machine
+enable_annette = True  # make the ANNETTE-latency-estimator available
