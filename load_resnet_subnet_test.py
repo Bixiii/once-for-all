be0cfd83f4ede9a36cf238175ac496b0efdc07b8
Birgit Schreiber <bixilein.fixilein@gmail.com>
2022-06-24 10:37:56 +0200

    Update test script for extracting a subnet

--- load_resnet_subnet_test.py
+++ load_resnet_subnet_test.py
@@ -1,6 +1,7 @@
 import torch
-from utils import count_flops_thop
+from utils import count_flops_thop, architecture_config_2_str
 from ofa.imagenet_classification.elastic_nn.networks import OFAResNets
+# from ofa.imagenet_classification.networks.resnets import *
 
 # define parameters for OFA-network ResNet50
 depth_list = [0, 1, 2]
@@ -19,22 +20,24 @@ ofa_network = OFAResNets(
     dataset='imagenet'
 )
 
-# test subnet config
+# subnet configurations for quick testing
 subnet_config_small = {'d': [0, 0, 2, 2, 2], 'e': [0.35, 0.25, 0.35, 0.25, 0.35, 0.2, 0.2, 0.25, 0.2, 0.2, 0.35, 0.2, 0.25, 0.2, 0.25, 0.35, 0.2, 0.2], 'w': [0, 1, 2, 0, 0, 0], 'image_size': 128}
 subnet_config_large = {'d': [2, 1, 2, 1, 1], 'e': [0.25, 0.35, 0.35, 0.25, 0.35, 0.35, 0.2, 0.2, 0.35, 0.25, 0.35, 0.35, 0.2, 0.25, 0.25, 0.35, 0.2, 0.25], 'w': [1, 0, 0, 2, 0, 2], 'image_size': 256}
 
-
 subnet_config = subnet_config_small
 
-# export
-path = 'ofa_resnet.pth'
-init = torch.load(path)
-ofa_network.load_state_dict(init)
-
 # get current subnet-config
+print('Extract subnet with config ' + str(subnet_config))
 ofa_network.set_active_subnet(d=subnet_config['d'], e=subnet_config['e'], w=subnet_config['w'])
 subnet = ofa_network.get_active_subnet()
 
+# define shape of the input tensor
 input_size = (1, 3, subnet_config['image_size'], subnet_config['image_size'])
+
+# count the flops of the subnetwork - to show that the forward pass works
 flops = count_flops_thop(subnet, input_size)
-print('flops: ' + str(flops))
+print(' -> MFLOPs: ' + str(flops))
+
+# TODO: here you can use the instance of the subnetwork
+x = torch.randn(*input_size, requires_grad=True).cpu()
+subnet.forward(x)
