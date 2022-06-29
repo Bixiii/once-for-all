3232ddc38308e2077a631f0d61b3a41352bb2318
Birgit Schreiber <bixilein.fixilein@gmail.com>
2022-06-29 18:49:50 +0200

    Remove absolute path

--- parse_load_resnet_subnet.py
+++ parse_load_resnet_subnet.py
@@ -1,9 +1,15 @@
+import argparse
+
 import torch
 from ofa.imagenet_classification.elastic_nn.networks import OFAResNets
 from result_net_configs import *
 from utils import *
 import csv
 from ofa.imagenet_classification.networks.resnets import ResNets
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file_path', type=str, default='')
+args = parser.parse_args()
 
 # define parameters for OFA-network ResNet50
 depth_list = [0, 1, 2]
@@ -23,7 +29,11 @@ ofa_network = OFAResNets(
 )
 
 # read configs for sub-networks from csv
-csv_file_path = r"D:\_Data\University\Master Thesis\Data\ofa_ncs2\resnet_subnet_metrics\metrics_ofa_resnet_subnets_20220609_11-01-27.csv"
+if not args.file_path:
+    csv_file_path = 'metrics_ofa_resnet_subnets_20220609_11-01-27.csv'
+else:
+    csv_file_path = args.file_path
+
 f = open(csv_file_path)
 csv_reader = csv.reader(f)
 
@@ -33,23 +43,19 @@ info_subnets = []
 for row in csv_reader:
     info_subnets.append({'file_id': row[-1], 'config': string_2_config_dict(row[0])})
 
-# import
-path = 'ofa_resnet.pth'
-init = torch.load(path)
-ofa_network.load_state_dict(init)
-
 # create subnet for each config
-for info_subnet in info_subnets[0:1]:
+for info_subnet in info_subnets:
 
     # get current subnet
     subnet_config = info_subnet['config']
     ofa_network.set_active_subnet(d=subnet_config['d'], e=subnet_config['e'], w=subnet_config['w'])
     subnet = ofa_network.get_active_subnet()
 
-    print('Metrics for file with id <' + info_subnet['file_id'] + '>')
     input_size = (1, 3, subnet_config['image_size'], subnet_config['image_size'])
-    flops = count_flops_thop(subnet, input_size)
-    print('flops: ' + str(flops))
 
-    # x = torch.randn(*input_size, requires_grad=True).cpu()
-    # subnet.forward(x)
+    # print('Metrics for file with id <' + info_subnet['file_id'] + '>')
+    # flops = count_flops_thop(subnet, input_size)
+    # print('flops: ' + str(flops))
+
+    x = torch.randn(*input_size, requires_grad=True).cpu()
+    subnet.forward(x)
